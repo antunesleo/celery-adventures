aea84c86123d858393e374681e225451f2b9d17c
Leonardo Antunes <antunesleo4@gmail.com>
2019-11-11 21:41:43 -0300

    added comments to first app

--- first_app/celery_stuff/tasks.py
+++ first_app/celery_stuff/tasks.py
@@ -1,11 +1,16 @@
 from time import sleep
 from celery import Celery
 
-app = Celery('celery_stuff', broker='redis://localhost/2')
+# Creating a celery instance.
+app = Celery('first_app', broker='redis://localhost/2')
 
 
 @app.task
 def serve_a_beer():
+    """
+     This is a celery task. Just a normal function with task decorator.
+     Note that we use the decorator from a celery insance.
+    """
     print('Getting a delicious beer!')
     sleep(3)
     print("""
--- first_app/first_app.py
+++ first_app/first_app.py
@@ -1,7 +1,11 @@
-from celery_stuff.tasks import serve_a_beer
+from celery_stuff.tasks import serve_a_beer # Importing the task
 
 
 def start_serve_a_beer():
+    """ Starts the execution of a celery task with the delay method.
+    the delay method doesn't wait the task execution be finished.
+    """
     serve_a_beer.delay()
+    print('This will be executed before the serve_a_beer task be finished')
 
 start_serve_a_beer()
